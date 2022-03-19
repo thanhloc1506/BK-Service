3f86d94439442a808e978df944538e4e9e21731b
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-03-20 01:12:01 +0700

    .

--- src/components/layouts/SearchResult.tsx
+++ src/components/layouts/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {Menu, Transition} from '@headlessui/react'
 import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
@@ -6,8 +6,10 @@ import {SearchResultItem} from "./SearchResultItem";
 
 export const SearchResult = ({show}: any) => {
     const state = useSelector((state: RootState) => state.search);
+    const [localShow, setLocalShow] = useState(true);
+
     return (
-        <div className="text-center fixed top-24 w-1/2 max-h-[70vh] overflow-scroll">
+        <div className="text-center fixed top-24 w-1/2 max-h-[70vh] overflow-scroll" hidden={!localShow}>
             <Menu as="div" className="relative text-left inline-block w-full ">
                 <Transition
                     show={state.isShowResult && state.status === "loading"}
@@ -15,6 +17,12 @@ export const SearchResult = ({show}: any) => {
                     enter="transition ease-out delay-1000"
                     enterFrom="transform opacity-0 scale-0"
                     enterTo="transform opacity-100 scale-100"
+                    afterLeave={()=>{
+                        if(!state.isShowResult) setLocalShow(false);
+                    }}
+                    beforeEnter={()=>{
+                        if(state.isShowResult) setLocalShow(true);
+                    }}
                 >
                     <div className={'flex justify-center fixed top-[10%] left-[50%] transform -translate-x-1/2 items-center p-10 transition-all'}>
                             <svg className="animate-spin h-10 w-10 bg-gray-100 self-center" viewBox="0 0 24 24">
@@ -31,6 +39,12 @@ export const SearchResult = ({show}: any) => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                     appear
+                    afterLeave={()=>{
+                        if(!state.isShowResult) setLocalShow(false);
+                    }}
+                    beforeEnter={()=>{
+                        if(state.isShowResult) setLocalShow(true);
+                    }}
                 >
                     {/*<Loading show={state.status !== "loading"}/>*/}
                     <div className={'w-full transition-all duration-300'}>
@@ -53,10 +67,7 @@ export const SearchResult = ({show}: any) => {
                         </Menu.Items>
                     </div>
                 </Transition>
-
             </Menu>
-
         </div>
-
     )
 }
