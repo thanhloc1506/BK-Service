daed6fa4b3ace0f2f1337239bd9e8d29e1986ce4
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-03-20 01:27:35 +0700

    .

--- src/components/layouts/SearchResult.tsx
+++ src/components/layouts/SearchResult.tsx
@@ -6,10 +6,8 @@ import {SearchResultItem} from "./SearchResultItem";
 
 export const SearchResult = ({show}: any) => {
     const state = useSelector((state: RootState) => state.search);
-    const [localShow, setLocalShow] = useState(true);
-
     return (
-        <div className="text-center fixed top-24 w-1/2 max-h-[70vh] overflow-scroll" hidden={!localShow}>
+        <div className={`text-center fixed top-24 w-1/2 max-h-[70vh] overflow-auto`}>
             <Menu as="div" className="relative text-left inline-block w-full ">
                 <Transition
                     show={state.isShowResult && state.status === "loading"}
@@ -17,12 +15,6 @@ export const SearchResult = ({show}: any) => {
                     enter="transition ease-out delay-1000"
                     enterFrom="transform opacity-0 scale-0"
                     enterTo="transform opacity-100 scale-100"
-                    afterLeave={()=>{
-                        if(!state.isShowResult) setLocalShow(false);
-                    }}
-                    beforeEnter={()=>{
-                        if(state.isShowResult) setLocalShow(true);
-                    }}
                 >
                     <div className={'flex justify-center fixed top-[10%] left-[50%] transform -translate-x-1/2 items-center p-10 transition-all'}>
                             <svg className="animate-spin h-10 w-10 bg-gray-100 self-center" viewBox="0 0 24 24">
@@ -39,12 +31,6 @@ export const SearchResult = ({show}: any) => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                     appear
-                    afterLeave={()=>{
-                        if(!state.isShowResult) setLocalShow(false);
-                    }}
-                    beforeEnter={()=>{
-                        if(state.isShowResult) setLocalShow(true);
-                    }}
                 >
                     {/*<Loading show={state.status !== "loading"}/>*/}
                     <div className={'w-full transition-all duration-300'}>
