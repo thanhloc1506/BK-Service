54fe899eddbefc7a6c9b405796b5e9095e5dba84
thanhloc1506 <locpooh1506@gmail.com>
2022-03-21 17:38:22 +0700

    merge code

--- src/components/rounting/ProtectedRoute.tsx
+++ src/components/rounting/ProtectedRoute.tsx
@@ -1,33 +1,13 @@
-import React, { useEffect } from "react";
-import { Route, useNavigate, Link, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
-import { loadUser } from "../../redux/slices/auth";
-import Loading from "../../views/Loading";
 import Homepage from "../../views/Homepage";
-
-interface IProps {
-  path: string;
-  Component: React.ComponentType<any>;
-}
 
 const ProtectedRoute = ({ children }: any) => {
   const auth = useSelector((state: RootState) => state.user);
 
-  const dispatch = useDispatch();
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(loadUser());
-  }, [dispatch]);
-
-  if (auth.authLoading) {
-    return <Loading />;
-  } else {
-    if (auth.isAuthenticated) return children;
-    return <Homepage />;
-  }
+  if (auth.isAuthenticated) return children;
+  return <Homepage />;
 };
 
 export default ProtectedRoute;
