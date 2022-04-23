caa9ca137e9cf04f779e8ff1450c959fdb5d4968
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-04-23 13:22:33 +0700

    .

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -49,7 +49,7 @@ const LoginForm: React.FC = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 bg-white/20 backdrop-blur" />
           </Transition.Child>
           {/* This element is to trick the browser into centering the modal contents. */}
           <span
