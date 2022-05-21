a583b73b0acbf7abf4410009f1274736262e5499
thanhloc1506 <locpooh1506@gmail.com>
2022-05-21 13:38:02 +0700

    add search rating

--- src/redux/slices/search.ts
+++ src/redux/slices/search.ts
@@ -1,14 +1,14 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {AxiosError, AxiosResponse} from "axios";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AxiosError, AxiosResponse } from "axios";
 import axiosClient from "../../apis/axios";
-import {PInSearch} from "../../apis/package/in/PInSearch";
-import {hideWaiting, showWaiting} from "./loading";
-import {Category} from "../../apis/common/Category";
-import {Phuong, Quan} from "../../apis/common/Address";
-import {PInCategory} from "../../apis/package/in/PInCategory";
-import {ADDRESS_API_URL} from "../../constants/common";
-import {PInQuan} from "../../apis/package/in/PInQuan";
-import {PInPhuong} from "../../apis/package/in/PInPhuong";
+import { PInSearch } from "../../apis/package/in/PInSearch";
+import { hideWaiting, showWaiting } from "./loading";
+import { Category } from "../../apis/common/Category";
+import { Phuong, Quan } from "../../apis/common/Address";
+import { PInCategory } from "../../apis/package/in/PInCategory";
+import { ADDRESS_API_URL } from "../../constants/common";
+import { PInQuan } from "../../apis/package/in/PInQuan";
+import { PInPhuong } from "../../apis/package/in/PInPhuong";
 
 export interface Filter {
   category?: Category;
@@ -24,7 +24,7 @@ export interface State {
   currentSearchText: string;
   currentQuickSearchText: string;
   dataSearch: PInSearch.Data | undefined;
-  dataQuickSeacrh: PInSearch.Data|undefined;
+  dataQuickSeacrh: PInSearch.Data | undefined;
   filter: Filter;
   //data
   quan: Quan[];
@@ -47,111 +47,133 @@ const initialState: State = {
   dataQuickSeacrh: undefined,
   currentQuickSearchText: "",
   page: 1,
-  totalPage: 1
+  totalPage: 1,
 };
-export const getAllCategory = createAsyncThunk("/getCategories", async (_, api) => {
-  const dispatch = api.dispatch;
-  dispatch(showWaiting())
-  return axiosClient.get<PInCategory>("/categories")
-      .then(res => res.data.categories)
+export const getAllCategory = createAsyncThunk(
+  "/getCategories",
+  async (_, api) => {
+    const dispatch = api.dispatch;
+    dispatch(showWaiting());
+    return axiosClient
+      .get<PInCategory>("/categories")
+      .then((res) => res.data.categories)
       .finally(() => dispatch(hideWaiting()));
-});
+  }
+);
 
 export const fetchQuan = createAsyncThunk("/getQuan", async (_, api) => {
   const dispatch = api.dispatch;
   dispatch(showWaiting());
-  return axiosClient.get<PInQuan>(ADDRESS_API_URL + "/province/district/79")
-      .then(res => res.data.results)
-      .finally(() => dispatch(hideWaiting()));
-})
-
-export const fetchPhuong = createAsyncThunk("/getPhuong", async (quanId: string, api) => {
-  const dispatch = api.dispatch;
-  dispatch(hideWaiting());
-  return axiosClient.get<PInPhuong>(ADDRESS_API_URL + "/province/ward/" + quanId)
-      .then(res => res.data.results)
-      .finally(() => dispatch(hideWaiting()));
+  return axiosClient
+    .get<PInQuan>(ADDRESS_API_URL + "/province/district/79")
+    .then((res) => res.data.results)
+    .finally(() => dispatch(hideWaiting()));
 });
 
-export const deepSearch = createAsyncThunk("/deepSearch", async (data: any|undefined, api: any) => {
-  const filter: Filter = api.getState().search.filter;
-  const dispatch = api.dispatch;
-  let text =  "";
-  let page = 1;
-  if(!data || !data.text){
-    text = api.getState().search.currentSearchText;
+export const fetchPhuong = createAsyncThunk(
+  "/getPhuong",
+  async (quanId: string, api) => {
+    const dispatch = api.dispatch;
+    dispatch(hideWaiting());
+    return axiosClient
+      .get<PInPhuong>(ADDRESS_API_URL + "/province/ward/" + quanId)
+      .then((res) => res.data.results)
+      .finally(() => dispatch(hideWaiting()));
   }
-  else{
-    text = data.text;
-  }
-  if(!data || !data.page){
-    page = api.getState().search.page;
-  }
-  else{
-    page = data.page;
-  }
-  let params: any = {
-    category: filter.category?._id,
-    quan: filter.quan?.district_id,
-    huyen: filter.phuong?.ward_id,
-    text: text,
-    page
-  }
-  Object.keys(params).forEach((k) => {
-    if (!params[k]) {
-      delete params[k];
+);
+
+export const deepSearch = createAsyncThunk(
+  "/deepSearch",
+  async (data: any | undefined, api: any) => {
+    const filter: Filter = api.getState().search.filter;
+    const dispatch = api.dispatch;
+    let text = "";
+    let page = 1;
+    if (!data || !data.text) {
+      text = api.getState().search.currentSearchText;
+    } else {
+      text = data.text;
     }
-  });
-  console.log(params)
-  dispatch(showWaiting());
-  return axiosClient.get<PInSearch.Data>("search", {
-    params
-  })
-      .then((res => res.data))
+    if (!data || !data.page) {
+      page = api.getState().search.page;
+    } else {
+      page = data.page;
+    }
+    let params: any = {
+      category: filter.category?._id,
+      quan: filter.quan?.district_id,
+      huyen: filter.phuong?.ward_id,
+      text: text,
+      page,
+    };
+    Object.keys(params).forEach((k) => {
+      if (!params[k]) {
+        delete params[k];
+      }
+    });
+    console.log(params);
+    dispatch(showWaiting());
+    return axiosClient
+      .get<PInSearch.Data>("search", {
+        params,
+      })
+      .then((res) => res.data)
       .finally(() => dispatch(hideWaiting()));
-});
+  }
+);
 
 export const search = createAsyncThunk("/search", async (text: string, api) => {
   const dispatch = api.dispatch;
   dispatch(showWaiting());
   return axiosClient
-      .get("search", {
-        params: {
-          text,
-        },
-      })
-      .then((res: AxiosResponse<PInSearch.Data>) => {
-        return res.data;
-      })
-      .catch((error: AxiosError) => {
-        throw error;
-      })
-      .finally(() => {
-        dispatch(hideWaiting());
-      });
+    .get("search", {
+      params: {
+        text,
+      },
+    })
+    .then((res: AxiosResponse<PInSearch.Data>) => {
+      return res.data;
+    })
+    .catch((error: AxiosError) => {
+      throw error;
+    })
+    .finally(() => {
+      dispatch(hideWaiting());
+    });
 });
 
-export const quickSearch = createAsyncThunk("/quickSearch", async (text: string, api: any) => {
-  const filter: Filter = api.getState().search.filter;
-  const dispatch = api.dispatch;
-  if(!text) text=api.getState().search.currentQuickSearchText;
-  let params: any = {
-    // category: filter.category?._id,
-    // quan: filter.quan?.district_id,
-    // huyen: filter.phuong?.ward_id,
-    text: text
-  }
-  Object.keys(params).forEach((k) => {
-    if (!params[k]) {
-      delete params[k];
-    }
-  });
-  return axiosClient.get<PInSearch.Data>("search", {
-    params
-  })
-      .then((res => res.data))
+export const quickSearch = createAsyncThunk(
+  "/quickSearch",
+  async (text: string, api: any) => {
+    const filter: Filter = api.getState().search.filter;
+    const dispatch = api.dispatch;
+    if (!text) text = api.getState().search.currentQuickSearchText;
+    let params: any = {
+      // category: filter.category?._id,
+      // quan: filter.quan?.district_id,
+      // huyen: filter.phuong?.ward_id,
+      text: text,
+    };
+    Object.keys(params).forEach((k) => {
+      if (!params[k]) {
+        delete params[k];
+      }
+    });
+    return axiosClient
+      .get<PInSearch.Data>("search", {
+        params,
+      })
+      .then((res) => res.data)
       .finally(() => {});
-});
+  }
+);
+
+export const selectRating = createAsyncThunk(
+  "/search/rating",
+  (rating: any) => {
+    return rating !== "Tất cả" ? rating : "Chọn rating";
+  }
+);
 
 const searchSlice = createSlice({
   name: "search",
@@ -166,7 +188,7 @@ const searchSlice = createSlice({
     setCurrentSearchText(state: State, action: PayloadAction<string>) {
       state.currentSearchText = action.payload;
     },
-    setCurrentQuickSearchText(state: State, action: PayloadAction<string>){
+    setCurrentQuickSearchText(state: State, action: PayloadAction<string>) {
       state.currentQuickSearchText = action.payload;
     },
     applyCategory(state: State, action: PayloadAction<Category>) {
@@ -187,14 +209,14 @@ const searchSlice = createSlice({
     resetCategory(state: State) {
       state.filter.category = undefined;
     },
-    clearQuickSearch(state: State){
+    clearQuickSearch(state: State) {
       state.dataQuickSeacrh = undefined;
-    }
+    },
   },
   extraReducers: {
     [search.fulfilled.toString()]: (
-        state,
-        action: PayloadAction<PInSearch.Data>
+      state,
+      action: PayloadAction<PInSearch.Data>
     ) => {
       const data = action.payload;
       state.status = "complete";
@@ -212,43 +234,67 @@ const searchSlice = createSlice({
     [search.rejected.toString()]: (state, action) => {
       state.status = "complete";
     },
-    [getAllCategory.fulfilled.toString()]: (state: State, action: PayloadAction<Category[]>) => {
+    [getAllCategory.fulfilled.toString()]: (
+      state: State,
+      action: PayloadAction<Category[]>
+    ) => {
       state.categories = action.payload;
     },
-    [getAllCategory.rejected.toString()]: (state: State, action: PayloadAction<any>) => {
+    [getAllCategory.rejected.toString()]: (
+      state: State,
+      action: PayloadAction<any>
+    ) => {
       state.categories = [];
     },
-    [fetchQuan.fulfilled.toString()]: (state: State, action: PayloadAction<Quan[]>) => {
+    [fetchQuan.fulfilled.toString()]: (
+      state: State,
+      action: PayloadAction<Quan[]>
+    ) => {
       state.quan = action.payload;
     },
     [fetchQuan.rejected.toString()]: (state: State, action: any) => {
       state.quan = [];
     },
-    [fetchPhuong.fulfilled.toString()]: (state: State, action: PayloadAction<Phuong[]>) => {
+    [fetchPhuong.fulfilled.toString()]: (
+      state: State,
+      action: PayloadAction<Phuong[]>
+    ) => {
       state.phuong = action.payload;
     },
-    [fetchPhuong.rejected.toString()]: (state: State, action: PayloadAction<any>) => {
+    [fetchPhuong.rejected.toString()]: (
+      state: State,
+      action: PayloadAction<any>
+    ) => {
       state.phuong = [];
     },
-    [deepSearch.fulfilled.toString()]: (state: State, action: PayloadAction<PInSearch.Data>) => {
+    [deepSearch.fulfilled.toString()]: (
+      state: State,
+      action: PayloadAction<PInSearch.Data>
+    ) => {
       let data = action.payload;
-      if (data.searchText === state.currentSearchText || (data.searchText === undefined && state.currentSearchText === "")) {
+      if (
+        data.searchText === state.currentSearchText ||
+        (data.searchText === undefined && state.currentSearchText === "")
+      ) {
         if (data.services.length > 0) {
           state.dataSearch = data;
-          state.totalPage=data.totalPage;
+          state.totalPage = data.totalPage;
           state.page = data.page;
         } else {
           state.dataSearch = undefined;
-          state.page= 1;
-          state.totalPage =1;
+          state.page = 1;
+          state.totalPage = 1;
         }
       }
     },
-    [quickSearch.fulfilled.toString()]: (state: State, action: PayloadAction<PInSearch.Data>) =>{
+    [quickSearch.fulfilled.toString()]: (
+      state: State,
+      action: PayloadAction<PInSearch.Data>
+    ) => {
       let data = action.payload;
       state.quickSearchStatus = "complete";
-      if(data.searchText === state.currentQuickSearchText){
-        console.log(data)
+      if (data.searchText === state.currentQuickSearchText) {
+        console.log(data);
         if (data.services.length > 0) {
           state.dataQuickSeacrh = data;
         } else {
@@ -256,14 +302,15 @@ const searchSlice = createSlice({
         }
       }
     },
-    [quickSearch.rejected.toString()]: (state: State, _)=>{
+    [quickSearch.rejected.toString()]: (state: State, _) => {
       state.quickSearchStatus = "complete";
     },
-    [quickSearch.pending.toString()]: (state: State, _)=>{
+    [quickSearch.pending.toString()]: (state: State, _) => {
       state.quickSearchStatus = "loading";
-    }
-
-
+    },
+    [selectRating.fulfilled.toString()]: (state, action) => {
+      state.filter.rating = action.payload;
+    },
   },
 });
 
@@ -278,7 +325,6 @@ export const {
   resetPhuong,
   resetCategory,
   resetQuan,
-    setCurrentQuickSearchText,
-  clearQuickSearch
-} =
-    searchSlice.actions;
+  setCurrentQuickSearchText,
+  clearQuickSearch,
+} = searchSlice.actions;
