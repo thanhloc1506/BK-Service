58ff2eec1a327dc651678f32be744ed01fccb13e
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-05-31 01:49:07 +0700

    build

--- src/redux/slices/search.ts
+++ src/redux/slices/search.ts
@@ -152,11 +152,11 @@ export const deepSearch = createAsyncThunk(
         const sortScore =
           (service.blogScore + (2.5 * (service.cmtScore + ratingScore)) / 2) /
             3.5 +
-          parseInt(enterpriseInfo.data.enterprise?.premium || "0");
+          parseInt(enterpriseInfo?.premium || "0");
 
         services.push({
           ...service,
-          enterprise: enterpriseInfo.data.enterprise,
+          enterprise: enterpriseInfo,
           ratingScore: ratingScore.toFixed(1),
           sortScore: sortScore.toFixed(1),
           rankingScore: rankingScore.toFixed(1),
@@ -252,11 +252,11 @@ export const quickSearch = createAsyncThunk(
         const sortScore =
           (service.blogScore + (2.5 * (service.cmtScore + ratingScore)) / 2) /
             3.5 +
-          parseInt(enterpriseInfo.data.enterprise.premium ?? "0");
+          parseInt(enterpriseInfo.premium ?? "0");
 
         services.push({
           ...service,
-          enterprise: enterpriseInfo.data.enterprise,
+          enterprise: enterpriseInfo,
           ratingScore: ratingScore.toFixed(1),
           sortScore: sortScore.toFixed(1),
           rankingScore: rankingScore.toFixed(1),
